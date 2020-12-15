cd88949f78770880fecbec90eb1979bf4276ac07
Bob German <bgerman1@hotmail.com>
2020-12-14 20:25:54 -0500

    Fixed hard-coded CRM URL for card images

--- Teams/cards/customerCard.js
+++ Teams/cards/customerCard.js
@@ -10,6 +10,10 @@ exports.getCard = function (customer) {
     // Create a Template instance from the template
     const template = new ACData.Template(card);
 
+    // Pass the CRM URL for image rendering
+    customer.crmUrl = `${process.env.CrmUrl}:${process.env.CrmPort}`;
+    console.log(customer.crmUrl);
+    
     // Expand the template with your `$root` data object.
     // This binds it to the data and produces the final Adaptive Card payload
     const ManagerCardLoad = template.expand({ $root: customer });
